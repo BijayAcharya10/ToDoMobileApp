adbe81f839e45268bfb60623cd1a27c548177945
BijayAcharya10 <bijaysharma2057@gmail.com>
2021-03-29 18:51:06 +0545

    create update delete and recycler view added

--- app/src/main/java/com/example/finaltodoapp/MainActivity.java
+++ app/src/main/java/com/example/finaltodoapp/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.finaltodoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.muddzdev.styleabletoast.StyleableToast;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    Fragment fragment;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        fragment = new ListTodoFragment();
+        fragmentManager.beginTransaction()
+                .replace(R.id.list_activity_container, fragment)
+                .commit();
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mnu_delete_comp:
+                StyleableToast.makeText(getApplicationContext(),"Completed Tasks Deleted",R.style.errorToast).show();
+                break;
+            case R.id.mnu_delete_all:
+                StyleableToast.makeText(getApplicationContext(),"All Tasks Deleted",R.style.errorToast).show();
+                break;
+            case R.id.mnu_logout:
+                SharedPreferences preferences = getApplicationContext().getSharedPreferences("todo_pref", 0);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear();
+                editor.commit();
+                Intent intent= new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
