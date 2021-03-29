b21a8864b761837736a6c844dfcbf500986a6dd1
BijayAcharya10 <bijaysharma2057@gmail.com>
2021-03-29 11:20:30 +0545

    shared preferences implemented in login and splash screen

--- app/src/main/java/com/example/finaltodoapp/LoginActivity.java
+++ app/src/main/java/com/example/finaltodoapp/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.NestedScrollView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -49,10 +50,18 @@ public class LoginActivity extends AppCompatActivity{
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(txtUsername.getText().toString().equals("bijay") && txtPassword.getText().toString().equals("bijay")){
+                if(txtUsername.getText().toString().equals("") && txtPassword.getText().toString().equals("")){
+                    StyleableToast.makeText(LoginActivity.this,"Please Enter Credentials",R.style.errorToast).show();
+                }
+                else if(txtUsername.getText().toString().equals("bijay") && txtPassword.getText().toString().equals("bijay")){
+                    SharedPreferences preferences = getApplicationContext().getSharedPreferences("todo_pref",0);
+                    SharedPreferences.Editor editor=preferences.edit();
+                    editor.putBoolean("authentication",true);
+                    editor.commit();
                     StyleableToast.makeText(LoginActivity.this,"LoggedIn Successfully",R.style.successToast).show();
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
+                    finish();
                 } else{
                     StyleableToast.makeText(LoginActivity.this,"Invalid Credentials..Try Again",R.style.errorToast).show();
                 }
--- app/src/main/java/com/example/finaltodoapp/SplashActivity.java
+++ app/src/main/java/com/example/finaltodoapp/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.finaltodoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -12,13 +13,24 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+       // this.getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                SharedPreferences preferences=getApplicationContext().getSharedPreferences("todo_pref",0);
+                Boolean authentication = preferences.getBoolean("authentication",false);
+                if(authentication){
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
             }
         },5000);
 
